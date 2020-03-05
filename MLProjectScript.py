24ce539fca2c79a62aa92b7dc221fc12fe54caef
project0451 <gspalletta@gmail.com>
2020-03-05 02:02:17 -0500

    First version of main script

---
+++ MLProjectScript.py
@@ -0,0 +1,7 @@
+# Python script for ML course project
+# import and clean data set on mammographic masses and run several ML models
+
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv("mammographic_maasses.data.txt")
