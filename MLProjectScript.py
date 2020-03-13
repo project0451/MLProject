66cac86584cda169cb459ec60d8ae07832019589
project0451 <gspalletta@gmail.com>
2020-03-12 21:00:46 -0400

    Added GridSearchCV parameter search and XGBoost random forest classifier

--- MLProjectScript.py
+++ MLProjectScript.py
@@ -20,10 +20,11 @@ df = pd.read_csv(
 # We don't actually need the BI-RADS data, and training the model on it would be cheating, so drop it
 df.drop(axis=1, labels="BI-RADS Assessment", inplace=True)
 
-# Create an Int64 with value pd.NA by hand because python insists it doesn't exist when I do it directly
-pdNAseries = pd.Series([None, 2, 4], dtype='Int64')
-print(pdNAseries)
-pdNA = pdNAseries[0]
+# Create pd.NA by hand because python insists it doesn't exist when I refer to it directly
+# May be related to changing "experimental" behavior of pd.NA and pandas nullable Int64
+# Currently appears to be using np.nan, which pd.NA was suppoedly made specifically to replace
+# This way should (hopefully) be relatively future-proof
+pdNA = pd.Series([None], dtype='Int64')[0]
 
 # Check for invalid values, and treat as missing data (NaN).  Valid values should be in these ranges
 # BI-RADS: 1-5 (ordinal)
@@ -71,15 +72,13 @@ tree_clf = tree.DecisionTreeClassifier()
 tree_clf.fit(x_train, y_train)
 
 # Report accuracy
-print("Accuracy:")
-print(tree_clf.score(x_test, y_test))
+print("Accuracy: %s" % tree_clf.score(x_test, y_test))
 
 # K-fold validation
 scores = model_selection.cross_val_score(tree_clf, data, target, cv=5)
-print("Validation:")
+print("Validation: %s" % scores)
 print(scores)
-print("Mean:")
-print(scores.mean())
+print("Mean: %s" % scores.mean())
 
 # Random forest using XGBoost
 # Split into training and test data and convert into XGBoost DMatrix containers
@@ -94,8 +93,7 @@ xgb_test = xgb.DMatrix(xgb_x_test, label=xgb_y_test, missing=-1)
 param = {
     'max_depth': 10,
     'eta': 0.25,
-    'objective': "multi:softmax",
-    'num_class': 2
+    'objective': "binary:hinge",
 }
 epochs = 10
 
@@ -104,5 +102,62 @@ xgb_model = xgb.train(param, xgb_train, epochs)
 
 # Evaluate model
 xgb_predictions = xgb_model.predict(xgb_test)
-print("XGBoost model accuracy:")
-print(accuracy_score(xgb_y_test, xgb_predictions))
+print("XGBoost model accuracy: %s" % accuracy_score(xgb_y_test, xgb_predictions))
+
+# Use model_selection.GridSearchCV to find optimal hyperparametr values
+# General syntax is:
+#   GridSearchCV(estimator, param_grid, scoring, n_jobs, cv, pre_dispatch)
+# where
+#   estimator = the model to apply GridSearchCV to
+#   param_grid = dictionary of hyperparamater names and lists of values
+#       {'param_1': [list of values], 'param_2': [list of values], etc.}
+#       will try every combination of values for every hyperparameter
+#   scoring = string or callable specifying scoring method to use
+#       uses estimator.score( ) by default if not specified
+#       try using 'accuracy' use metrics.accuracy_score for a classifier
+#   n_jobs = maximum number of parallel jobs, 1 if not specified
+#       -1 for all available processors, -n for all but (n-1) processors
+#   cv = number of cross-validation folds, uses 5-fold if not spedified
+#   pre_dispatch = limit number of copies of data in memory if n_jobs !=1, default is '2*n_jobs'
+#
+# Also need the scikit-learn wrapper class for XGBoost
+# xgb.XGBClassifier(objective='binary:logistic',)
+# xgb.XGBRFClassifier(learning_rate=1,)
+# For this case, we want objetive='binary:hinge'
+# Relevant parameters
+#   n_estimators = number of trees in random forest to fit(RF only)
+#   max_depth = maximum tree depth for base learners
+#   learning_rate = (float) "eta" in xgb, boosted learning rate
+#   objective = learning task and objective, or custom objective function
+#   booster = 'gbtree', 'gblinear', or 'dart'
+#   tree_method = ??? (leave as default for now)
+#   n_threads = number of threads to use
+#   gamma = (float) minimum loss reduction requared to make a furhter partition
+#   min_child_seight = minimum sum of instance weight needed in a child
+#   missing = value to treat as missing
+#   num_parallel_tree = used for random forest
+
+xgb_rf_model = xgb.XGBRFClassifier(objective="binary:hinge", missing=-1)
+xgb_param_grid = {
+    "n_estimators": [50,100,200],
+    "max_depth": [4,6,8,10,12],
+    "learning_rate": [0.2, 0.3, 0.5, 0.75, 1, 1.25, 1.5, 2],
+    "num_parallel_tree": [50, 100, 200],
+    "gamma": [0,0.1,0.25],
+    "min_child_weight": [0.5,1,2]
+}
+
+xgb_grid_rf_clf = model_selection.GridSearchCV(xgb_rf_model, xgb_param_grid, n_jobs=4)
+#xgb_grid_rf_clf = xgb_rf_model
+xgb_grid_rf_clf.fit(xgb_x_train, xgb_y_train)
+
+print("")
+print(xgb_grid_rf_clf.best_score_)
+print("")
+print(xgb_grid_rf_clf.best_params_)
+
+print("Accuracy: %s" % xgb_grid_rf_clf.score(xgb_x_test, xgb_y_test))
+scores = model_selection.cross_val_score(xgb_grid_rf_clf, df_xgb.drop(axis=1, labels=['Severity']), df_xgb['Severity'])
+print("Cross Validation: %s" % scores)
+print("Mean: %s" % scores.mean())
+print(xgb_grid_rf_clf.get_params())
